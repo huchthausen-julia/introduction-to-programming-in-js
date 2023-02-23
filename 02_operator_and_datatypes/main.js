4170a3a54b0ee39e47568843607fc27ad159dc4c
Julia Huchthausen <huchthausenjulia@gmail.com>
2023-02-23 18:23:05 +0100

    add task operators & data types

---
+++ 02_operator_and_datatypes/main.js
@@ -0,0 +1,29 @@
+
+// 01) Write a String that logs out your first name
+console.log("J" + "U" + "L" + "I" + "A");
+
+// 02) Spot the problems & solve them
+// Copy the following code snippets to the script.js file and try to spot and fix the errors.
+
+// Error! Make it work
+console.log("Hallo");
+
+// Error! Make it work
+console.log("That doesn`t work");
+
+// should return true. Change only the operator to fix it.
+console.log(50 + 2 >= 52);
+
+// should be false. Change only the operator to fix it.
+console.log("333" === 333);
+
+// Wrong result: Expected 555 not 855. Change only the operator to fix it.
+console.log((600 + 510) / 2);
+
+// Wrong result: Expected a Number 4 received 22
+console.log(2 + 2);
+
+// 03) Compare two conditions
+// Create a console.log that returns true when both division results are equal to 5
+console.log(25 / 5 == 50 / 10); // Result: 5
+console.log(50 / 10 >= 25 / 5); // Result: 5
