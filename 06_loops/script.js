6058ae508a58718c3ea081e22bea1e3a8b9d4310
Julia Huchthausen <huchthausenjulia@gmail.com>
2023-03-11 17:53:44 +0100

    update_05_06

---
+++ 06_loops/script.js
@@ -0,0 +1,20 @@
+
+// 1) oddNumbers function
+// Implement a oddNumbers function.
+
+//  -> The function has two parameters, both are numbers.
+//  -> Iterate over all numbers between first number and the second number.
+//  -> Store all oddNumbers in string, seperated by a comma and return the string.
+//  -> Allowed are only positive numbers, add a check.
+
+function oddNumbers(num1, num2) {
+    
+    for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+        
+    }
+}
+
+
+console.log(oddNumbers(0, 4));
+// result should be: 1,3
